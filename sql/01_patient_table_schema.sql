0fdcbb60157eb430fd94434c76d6abd557a38025
Laive <laiveetom@gmail.com>
2026-09-11 04:59:51 +0100

    Created the table schema for storing patient data

---
+++ sql/01_patient_table_schema.sql
@@ -0,0 +1,16 @@
+/*Table schema for storing the patient records from the comma-separated-values*/
+CREATE TABLE patients (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, --autoincrementing id of the table
+    age INT, -- age of the patient
+    gender VARCHAR(20), -- gender of the patient
+    state VARCHAR(50), -- state of origin of the patient
+    diagnosis VARCHAR(100), -- medical diagnosis recored
+    admission_date DATE, -- date of hospital admission
+    discharge_date DATE, -- date the patient was discharged or expected to be discharged
+    admission_type VARCHAR(50), -- type of hospital admission
+    department VARCHAR(100), -- department where the patient was admitted
+    length_of_stay INT, -- length of stay in the hospital
+    treatment_cost INT, -- cost of the treatment
+    insurance_status VARCHAR(100), -- insurance of the patient
+    outcome VARCHAR(50) -- treatment outcome
+);
